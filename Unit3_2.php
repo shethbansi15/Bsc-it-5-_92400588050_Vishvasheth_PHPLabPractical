c6fb061fcc795718ef8918dd27747edc8721644d
shethbansi15 <shethbansi15@gmail.com>
2026-08-04 08:51:48 +0530

    Add files via upload

---
+++ Unit3_2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+        <form action="itu3p14_2Cookie.php" method="post">
+
+        <input type ="text" name="username"><br><br>
+
+        <input type="submit" name="create" value="Create Cookie"> |
+        <input type="submit" name="create" value="Create Cookie"> |
+    <?php
+
+      
+    ?>
+</body>
+</html>
